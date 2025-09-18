ef169023ee6d92706ffe818c5e0b20909db3cf60
Ankit <jswlakt@gmail.com>
2025-09-19 00:00:07 +0530

    add urls

--- expense_tracker/expense_tracker/urls.py
+++ expense_tracker/expense_tracker/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from expenses import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.expense_list, name='expense_list'),
+    path('add/', views.add_expense, name='add_expense'),
 ]
+
