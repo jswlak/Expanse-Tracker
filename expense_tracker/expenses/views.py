3ef520069ce16ef7888d879181ca298b9c5254be
Ankit <jswlakt@gmail.com>
2025-09-18 23:58:15 +0530

    add views and forms

--- expense_tracker/expenses/views.py
+++ expense_tracker/expenses/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Expense
+from .forms import ExpenseForm
 
-# Create your views here.
+def expense_list(request):
+    expenses = Expense.objects.all().order_by('-date')
+    total = sum(exp.amount for exp in expenses)
+    return render(request, 'expenses/expense_list.html', {'expenses': expenses, 'total': total})
+
+def add_expense(request):
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('expense_list')
+    else:
+        form = ExpenseForm()
+    return render(request, 'expenses/add_expense.html', {'form': form})
