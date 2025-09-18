f351839289c1e1d1aa1052add488f2a5ac008e15
Ankit <jswlakt@gmail.com>
2025-09-18 23:53:33 +0530

    add models

--- expense_tracker/expenses/models.py
+++ expense_tracker/expenses/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Expense(models.Model):
+    CATEGORY_CHOICES = [
+        ('Food', 'Food'),
+        ('Travel', 'Travel'),
+        ('Shopping', 'Shopping'),
+        ('Bills', 'Bills'),
+        ('Other', 'Other'),
+    ]
+
+    title = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.amount}"
